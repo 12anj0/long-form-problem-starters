f457fe8c3a144b8074c11102db1a7337c72cde20
Ranjan Singh <ranjansingh233@gmail.com>
2024-04-06 23:13:23 +0530

    Create BeansAmountException.java

---
+++ CoffeeMaker/src/model/BeansAmountException.java
@@ -0,0 +1,17 @@
+package model;
+
+public class BeansAmountException extends Exception{
+    private double beans;
+
+    public BeansAmountException(double beans) {
+        this.beans = beans;
+    }
+
+    public double getBeans() {
+        return beans;
+    }
+
+    public void setBeans(double beans) {
+        this.beans = beans;
+    }
+}
